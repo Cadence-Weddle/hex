4479482162968c695f5a0d742e0304fcf6c432cf
Cadence Weddle <cadenceweddle@gmail.com>
2017-12-19 10:00:39 -0500

    Removed outdated functions

--- AIs/Cadence/GameTree.pyx
+++ AIs/Cadence/GameTree.pyx
@@ -109,73 +109,12 @@ class Tree:
 			self.terminal_nodes = [i for i in iter_flatten(self.terminal_nodes.append(nodes))]
 		return nodes
 
-	def convert_board(self, b, c):
-		for x, item in enumerate(b):
-			b[x] = c[item]
-		return b
-
 	def update_root_node(self, new_root_node):
 		self.root_node = new_root_node
 		self.depth -= 1
+		self.root_node.update_depth(1)
+	
 
 
 
 
-
-
-
-
-'''
-cdef full_alpha_beta(cdef Node node, cdef Node alpha, cdef Node beta, cdef int curr_depth, cdef int max_depth,
-		cdef bool maximising):
-	if depth == max_depth:
-		if not cache.check(node):
-			node.score = score(node)
-		return node
-	cdef Node value
-	elif maxmising:
-	 for subnode in node.subnodes:
-		value  = Node(None, None, None, float("inf"))
-		value = max_node(value, full_alpha_beta_tree_search((alpha, beta, subnode, depth + 1, max_depth, False))		
-		if beta <= alpha:
-			break
-	 node.score = value
-	 return value
-
-	elif not maxmising:
-		next_layer = node.subnodes
-	 for subnode in next_layer:
-		value  = Node(None, None, None, float("inf"))
-		value = min_node(value, full_alpha_beta_tree_search((alpha, beta, subnode, depth + 1, max_depth, True))		
-		if beta <= alpha:
-			break
-	node.score = value
-	return value
-
-
-cdef best_alpha_beta(node, LONG alpha, LONG beta, int curr_depth, int max_depth,
-		bool maximising, int num_best_nodes):
-	if depth == max_depth:
-		Node.score = score(Node)
-		return Node
-	node.sort_subnodes()
-	elif maxmising:
-		next_layer = node.subnodes[:num_best_nodes]
-	 for subnode in next_layer:
-		value  = Node(None, None, None, - float("inf"))
-		value = max_node(value, full_alpha_beta_tree_search((alpha, beta, subnode, depth + 1, max_depth, False))		
-		
-		if beta <= alpha:
-			break
-	 node.score=value
-	 return value
-	elif not maxmising:
-		next_layer = node.subnodes[-1:len(node.subnodes) - num_best_nodes:-1]
-	 for subnode in next_layer:
-		value  = Node(None, None, None, float("inf"))
-		value = min_node(value, full_alpha_beta_tree_search((alpha, beta, subnode, depth + 1, max_depth, True))		
-		if beta <= alpha:
-	 	 break
-	 node.score = value
-	 return value
-'''
