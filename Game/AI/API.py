e8825029e20affb9231cf10307879ee339606267
Jonathan Z <yuyangzhou03@gmail.com>
2018-03-12 14:40:13 -0400

    fixes

--- Game/AI/API.py
+++ Game/AI/API.py
@@ -1,13 +1,6 @@
 import random
-<<<<<<< HEAD
 import numpy as np
 
 def MakeMove(computetime,board,humanplayer):
-	return {'moveloc':random.choice(np.argwhere(np.array(board)==0)[0]),'gamestate':0}
-=======
 
-def MakeMove(computetime,board,humanplayer):
-    return {'moveloc':random.choice([x for x in range(121)]),'gamestate':0}
-
-
->>>>>>> 110e644cf50c230719c4f10080818f5bb4027709
+	return {'moveloc':int(random.choice(np.argwhere(np.array(board)==0))[0]),'gamestate':0}
