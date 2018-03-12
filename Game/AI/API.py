cb9a323fab5c266124d669ff49e2317174230c81
Jonathan Z <yuyangzhou03@gmail.com>
2018-03-12 16:51:51 -0400

    Small Change

--- Game/AI/API.py
+++ Game/AI/API.py
@@ -1,6 +1,6 @@
 import random
 import numpy as np
-import AI.GameLogic.GameLogic
+import AI.GameLogic.GameLogic as GameLogic
 
 def MakeMove(computetime,board,humanplayer):
 	return {'moveloc':random.choice(GameLogic.GetValidMoves(np.array(board))),'gamestate':0}
