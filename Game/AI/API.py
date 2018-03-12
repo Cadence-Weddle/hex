74e3469b722037de074a0fd5ee689957c5c48a20
Jonathan Z <yuyangzhou03@gmail.com>
2018-03-12 13:03:51 -0400

    latest updates

--- Game/AI/API.py
+++ Game/AI/API.py
@@ -1,2 +1,5 @@
+import random
+import numpy as np
+
 def MakeMove(computetime,board,humanplayer):
-    return {'moveloc':85,'gamestate':0}
+	return {'moveloc':random.choice(np.argwhere(np.array(board)==0)[0]),'gamestate':0}
--- Game/WebServer.py
+++ Game/WebServer.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template, request, jsonify
 from AI.API import MakeMove
 import time
+import datetime
 import numpy as np
 
 app = Flask(__name__)
@@ -12,10 +13,9 @@ def hexgame():
 @app.route('/_processrequest', methods=['GET', 'POST'])
 def processrequest():
 	indata = request.get_json()
-	print(indata,time.time())
+	print('Request Recived at {time}:{data}'.format(time=datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S.%f'),data=indata))
 	output =  MakeMove(computetime = indata['computetime'],board = np.array(indata['board']), humanplayer = indata['humanplayer'])
-	print(output,time.time())
-	print(type(indata['board']))
+	print('Response at {time}:{data}'.format(time=datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S.%f'),data=output))
 	return jsonify(**output)
 
 if __name__ == "__main__":
