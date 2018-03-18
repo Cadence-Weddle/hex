d63647101dbd17e5b8fa9158557e2a2ca87d608a
Jonathan Z <yuyangzhou03@gmail.com>
2018-03-18 15:13:03 -0400

    Minor Update

--- Game/AI/API.py
+++ Game/AI/API.py
@@ -4,16 +4,22 @@ sys.path.append("C:\\Users\\user\\Desktop\\Github\\hex.github.io\\Game\\AI\\")
 import random
 import numpy as np
 import AI.GameLogic.GameLogic as GameLogic
-from TreeSeach.MonteCarloTreeSearch import MonteCarloTreeSearch as MCTS
-from NeuralNetwork.NeuralNetwork import NeuralNetwork as NN
+import AI.TreeSearch.MonteCarloTreeSearch as MonteCarloTreeSearch
+import AI.NeuralNetwork.NeuralNetwork as NeuralNetwork
+
+MCTS = MonteCarloTreeSearch.MonteCarloTreeSearch
+NN = NeuralNetwork.NeuralNetwork
+
 def _MakeMove(computetime,board,humanplayer):
 	return {'moveloc':str(np.random.choice([x for x in range(121)])),'gamestate':str(np.random.choice([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,2]))}
 
 
 def MakeMove(computetime, board, humanplayer,):
 		Game = GameLogic.Game()
-		Game.board = [x if x == 1 or x == 0 else -1 for x in board]
+		Game.board = np.array([x if x == 1 or x == 0 else -1 for x in board])
 		Game.NextPlayer = -1
 		Game.InvertBoard()
 		mcts = MCTS(Game, NN())
-		return {'moveloc' : MCTS.turn(10), 'gamestate': str(np.random.choice([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,2]))}
+		move = mcts.turn(11)
+		Game.MakeMove(move)
+		return {'moveloc' : str(move), 'gamestate': str(Game.GameState)}
