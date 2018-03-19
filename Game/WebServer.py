6f615411318f20cdd0ee5981fd74d941c09b7a24
Jonathan Z <yuyangzhou03@gmail.com>
2018-03-18 21:57:54 -0400

    Optimized and made working Gen_history function

--- Game/WebServer.py
+++ Game/WebServer.py
@@ -32,23 +32,16 @@ class MCTS_Manager:
 		move = mcts.turn(computetime)
 		RootNodePointer[move].parent = RootNodePointer
 		return {'moveloc' : str(move), 'gamestate' : str(RootNodePointer[move].game.GameState)}
-
-	def gen_history(self, board):
-		"""
-		Returns a possible history of moves that could have led to that state. 
-		"""
-		history = []
-		p1 = [x for x,i in enumerate(board) if i == 1]
-		p2 = [x for x,i in enumerate(board) if i == 2]
-		i = 0
-		while p1 and p2:
-			history.append(p1[i])
-			try:
-				history.append(p2[i])
-			except:
-				break
-			i += 1
-		return history
+	
+	def gen_history(board):
+	    """
+	    Returns a possible history of moves that could have led to that state. 
+	    """
+	    board = np.array(board)
+	    p1 = np.where(board == 1)
+	    p2 = np.where(board == 2)
+	    lesser = min(len(p1),len(p2))
+	    return np.stack((p2[0][:lesser],p1[0][:lesser])).flatten('F')
 
 
 app = Flask(__name__)
@@ -58,17 +51,17 @@ MCTSM = MCTS_Manager()
 def hexgame():
 	return render_template('Hexgame.html')
 
-@app.route('/_processrequest', methods=['GET', 'POST'])
+@app.route('/processrequest', methods=['GET', 'POST'])
 def processrequest():
 	indata = request.get_json()
+	curr_board = np.array(indata['board'])
 	#Check if Game Has Reached Terminal State
-	if GameLogic.GetGameState(indata['board'])==0:
+	if GameLogic.GetGameState(curr_board)==0:
 		return jsonify({'gamestate':0})
 	else:
-		output =  MCTSM.MakeMove(computetime = indata['computetime'], board = indata['board'])
+		output =  MCTSM.MakeMove(computetime = indata['computetime'], board = curr_board)
 		print('Response at {time}:{data}'.format(time=datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S.%f'),data="-"))
 		return jsonify(**output)
-
 
 if __name__ == "__main__":
 	app.run(port=80,host= '0.0.0.0', threaded=True)
